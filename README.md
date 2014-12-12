330d572b7339e3cf1902c53cb7c0413a80b7af8f
Abner Campanha <abnerpc@fastmail.com>
2014-12-11 23:11:18 -0200

    changes in the script and readme

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 noleito
 =======
 
-python script to check bus seats
+Python script to check only the configured available bus seats for the website http://www.expressodeprata.com.br
+
+Configure the config.json file with the cities, seats and times
+
+Run the app:
+
+```python src/run.py```
--- src/app.py
+++ src/app.py
@@ -13,46 +13,53 @@ class App(object):
 
     def __init__(self):
         self.data = {}
-        self.services = {}
-
-    def _load_data(self, json_data):
-        for field in settings.FILE_REQUIRED_FIELDS:
-            _value = json_data.get(field, None)
-            if _value:
-                self.data[field] = _value
 
     def _is_data_valid(self):
+        """
+        Check if the data loaded from config file is valid
+        """
+
+        # check if all required fields are present
         _fields = (field in self.data
                    for field in settings.FILE_REQUIRED_FIELDS)
         if not all(_fields):
             return False
-        _values = '{}:{}'.format(self.data['first_time'].strip(),
-                                 self.data['last_time'].strip()).split(':')
-        if not all(_values) or len(_values) < 4:
+
+        # check if all time fields are valid numbers
+        _times = ''.join(self.data['first_time'].split(':') +
+                         self.data['last_time'].split(':'))
+        if not _times or len(_times) < 8:
             return False
         try:
-            for _v in _values:
-                int(_v)
+            int(_times)
         except ValueError:
             return False
+
         return True
 
     def _get_date(self, _hour, _minute):
+        """
+        Returns a datetime object with the hour and minute provided
+        """
         return datetime.now().replace(hour=int(_hour),
                                       minute=int(_minute),
                                       second=0,
                                       microsecond=0)
 
     def _load_configured_times(self):
-
+        """
+        Put the first and last time in an instance variable
+        """
         _h, _m = self.data['first_time'].split(':')
         self.first_time = self._get_date(_h, _m)
 
         _h, _m = self.data['last_time'].split(':')
         self.last_time = self._get_date(_h, _m)
 
-    def _is_time_type_in_data(self, _time, _type):
-
+    def _is_time_and_type_valid(self, _time, _type):
+        """
+        Checks if the times and types provided in the config file are valid
+        """
         if _time is None or _type is None:
             return False
 
@@ -66,10 +73,16 @@ class App(object):
         return (self.first_time <= current_time <= self.last_time)
 
     def _is_tds_valid(self, tds):
+        """
+        Check for valid tds name
+        """
         return (tds and len(tds) > 0 and tds[0].input and
                 tds[0].input['name'] == 'srv_ida')
 
     def _get_services_data(self):
+        """
+        Returns only the services that matches the configuration file
+        """
 
         params = {
             'txt_desde': self.data['city_from'],
@@ -83,29 +96,34 @@ class App(object):
         services_data = []
 
         for city in cities:
+
             params['txt_hasta'] = city
             soup = BeautifulSoup(requests.post(settings.MAIN_URL, params).text)
             trs = soup.find_all('tr', re.compile("fila_"))
+
             for tr in trs:
+
                 tds = tr.find_all('td')
                 if not self._is_tds_valid(tds):
                     continue
-                _time, _type = tds[1].text, tds[5].text
-                _time = _time.strip()
-                _type = _type.strip()
 
-                if self._is_time_type_in_data(_time, _type):
+                _time, _type = tds[1].text.strip(), tds[5].text.strip()
+
+                if self._is_time_and_type_valid(_time, _type):
                     services_data.append(
                         {
                             'city_to': city,
+                            'time': _time,
                             'seats': self.data['types'][_type],
                             'service': tds[0].input['value']
                         })
 
         return services_data
 
-    def _get_seats_data(self, services_data):
-
+    def _get_available_seats(self, services_data):
+        """
+        Returns an array with the details of matched seats available
+        """
         params = {
             'desde': self.data['city_from'],
             'fecha': self.data['date'],
@@ -114,25 +132,30 @@ class App(object):
             'enviar': ''
         }
 
-        seats = []
+        availables = []
 
         for service_data in services_data:
+
             params['hasta'] = service_data['city_to']
             params['srv_ida'] = service_data['service']
             soup = BeautifulSoup(requests.post(settings.MAIN_URL, params).text)
             trs = soup.find_all('td', re.compile("libre"))
-            for tr in trs:
-                if tr.text in service_data['seats'].split(';'):
-                    seats.append(tr.text)
+            seats = [tr.text for tr in trs
+                     if tr.text in service_data['seats'].split(';')]
+            if len(seats) > 0:
+                service_data['seats'] = seats
+                availables.append(service_data)
 
-        return seats
+        return availables
 
     def run_app(self):
+        """
+        Start point for the app execution
+        """
 
         file_path = join(settings.FILE_ROOT, settings.FILE_NAME)
-        json_data = json.load(open(file_path))
+        self.data = json.load(open(file_path))
 
-        self._load_data(json_data)
         if not self._is_data_valid():
             print 'invalid config'
             return
@@ -143,7 +166,7 @@ class App(object):
             print 'services data not found'
             return
 
-        seats_data = self._get_seats_data(services_data)
+        seats_data = self._get_available_seats(services_data)
         if not seats_data:
             print 'seats not found'
             return
